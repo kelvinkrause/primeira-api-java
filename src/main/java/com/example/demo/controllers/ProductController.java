56b14100ef0d9b5c1647858f2b203e4b596daa52
KelvinKrause <kelvinrkrause@outlook.com>
2024-08-19 22:09:16 -0300

    Ajustado o metodo Post e criado o metodo Delete

--- src/main/java/com/example/demo/controllers/ProductController.java
+++ src/main/java/com/example/demo/controllers/ProductController.java
@@ -3,10 +3,14 @@ package com.example.demo.controllers;
 import com.example.demo.domain.product.Product;
 import com.example.demo.domain.product.ProductRepository;
 import com.example.demo.domain.product.RequestProduct;
+import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.beans.Transient;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/product")
@@ -29,11 +33,23 @@ public class ProductController {
     }
 
     @PutMapping
+    @Transactional
     public ResponseEntity updateProduct(@RequestBody @Valid RequestProduct data) {
-        Product product = repository.getReferenceById(data.id());
-        product.setName(data.name());
-        product.setPrice_in_cents(data.price_in_cents());
-        return ResponseEntity.ok(product);
+        Optional optionalProduct = repository.findById(data.id());
+        if (optionalProduct.isPresent()){
+            Product product = (Product) optionalProduct.get();
+            product.setName(data.name());
+            product.setPrice_in_cents(data.price_in_cents());
+            return ResponseEntity.ok(optionalProduct);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteProduct(@PathVariable String id){
+        repository.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 
 
